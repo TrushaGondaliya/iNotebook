b4e7e73be8e15154b7329c29ea38a54fb1b514fb
TrushaGondaliya <trushagondaliya30@gmail.com>
2025-10-22 15:14:00 +0530

    Added validation

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoos = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017';
+const mongoURI = 'mongodb://localhost:27017/inotebook';
 
 const connectToMongo = () => {
     mongoos.connect(mongoURI);
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 // Create a user using: POST "api/auth". Doesn't require Auth (no need to login)
-router.post('/', (req, res) => {
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name', 'Enter valid name').isLength({min: 2}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 character').isLength({min: 5})
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+        res.json({error: 'Please enter a unique email', message: err.message})
+    });
 })
 
 module.exports = router;
